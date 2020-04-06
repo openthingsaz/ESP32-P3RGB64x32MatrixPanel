aa85fe9ffd7083426e31cebe634482d5c7f19f08
openthingsaz <openthingsaz@gmail.com>
2020-04-06 16:25:13 +0900

    블루투스 이름 변경

--- examples/ledmatrix_32x64/ledmatrix_32x64.ino
+++ examples/ledmatrix_32x64/ledmatrix_32x64.ino
@@ -119,11 +119,11 @@ BluetoothSerial SerialBT;
 void setup() {
 
   Serial.begin(115200);
-  SerialBT.begin("esp3205"); //Bluetooth device name
+  SerialBT.begin("ledmatrix"); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 
   matrix.begin();
-  /*
+  
   // draw a pixel in solid white
   matrix.drawPixel(0, 0, matrix.color444(5, 5, 5)); 
   delay(500);
@@ -196,7 +196,7 @@ void setup() {
   matrix.print("B");
   matrix.setTextColor(matrix.color444(5,0,8)); 
   matrix.println("*");
-
+/*
   delay(2000);
   for (int i=0; i<64*30; i++)
     matrix.matrixbuff[i] = 0x333;
@@ -222,6 +222,8 @@ void setup() {
   for (int i=0; i<64*30; i++)
     matrix.matrixbuff[i] = 0x800;
   */
+
+  
   /*
   delay(2000);
   for (int i=0; i<64*30; i++)
@@ -290,26 +292,9 @@ void loop() {
     Serial.print(temp[0], DEC);
     Serial.println();
     cmd = temp[0] - 48;
-    if(len == 1) {
-      if(cmd == 1)
-      {
-        // draw a pixel in solid white
-        matrix.drawPixel(0, 0, matrix.color444(7, 7, 7));
-        delay(500);
-      }
-      else if(cmd == 2)
-      {
-        // fix the screen with green
-        matrix.fillRect(0, 0, matrix.width(), matrix.height(), matrix.color444(0, 0, 2));
-        delay(500);
-      }
-      else if(cmd == 3)
-      {
-        // draw a box in yellow
-        matrix.drawRect(0, 0, matrix.width(), matrix.height(), matrix.color444(7, 7, 0));
-        delay(500);
-      }
-      else if(cmd ==4)
+    if(len == 1) 
+    {
+      if(cmd ==4)
       {
         // draw an 'X' in red
         matrix.drawLine(0, 0, matrix.width()-1, matrix.height()-1, matrix.color444(7, 0, 0));
@@ -344,26 +329,28 @@ void loop() {
         
         // print each letter with a rainbow color
         matrix.setTextColor(matrix.color444(7,0,0));
-        matrix.print('3');
+        matrix.print('0');
         matrix.setTextColor(matrix.color444(7,4,0));
-        matrix.print('2');
+        matrix.print('1');
         matrix.setTextColor(matrix.color444(7,7,0));
-        matrix.print('x');
+        matrix.print('0');
         matrix.setTextColor(matrix.color444(4,7,0));
-        matrix.print('6');
+        matrix.print('4');
         matrix.setTextColor(matrix.color444(0,7,0));
         matrix.print('4');
-        matrix.setCursor(34, 24);
+        //matrix.setCursor(34, 24);
         matrix.setTextColor(matrix.color444(0,7,7));
-        matrix.print('*');
+        matrix.print('1');
         matrix.setTextColor(matrix.color444(0,4,7));
-        matrix.print('R');
+        matrix.print('8');
         matrix.setTextColor(matrix.color444(0,0,7));
-        matrix.print('G');
+        matrix.print('4');
         matrix.setTextColor(matrix.color444(4,0,7));
-        matrix.print('B');
+        matrix.print('4');
         matrix.setTextColor(matrix.color444(7,0,4));
-        matrix.print('*');
+        matrix.print('5');
+        matrix.setTextColor(matrix.color444(7,0,4));
+        matrix.print('1');
       }
       else if(cmd ==6)
       {
@@ -391,8 +378,148 @@ void loop() {
         //matrix.println("LED MATRIX!");
    
       }
-      
-    }
+      else if(cmd == 7)
+      {
+        // fill the screen with 'black'
+        matrix.fillScreen(matrix.color444(0, 0, 0));
+        // draw some text!
+        matrix.setTextSize(1);     // size 1 == 8 pixels high
+        matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+        
+        matrix.setCursor(5, 0);    // start at top left, with 8 pixel of spacing
+        uint8_t w = 0;
+        int len1;
+        char *str = "WelcometoOurCompany";
+        len1 = strlen(str);
+        for (w=0; w<len1; w++) 
+        {
+          if(w == 9) matrix.println();
+          matrix.setTextColor(Wheel(w));
+          matrix.print(str[w]);  
+        }
+        matrix.println();
+        matrix.setTextColor(matrix.color444(7,7,7));
+        matrix.println("Lunch Time");
+        //matrix.println("Call me");
+        
+        print_phone_num();
+      }
+      else if(cmd == 8)
+      {
+         // fill the screen with 'black'
+          matrix.fillScreen(matrix.color444(0, 0, 0));
+          // draw some text!
+          matrix.setTextSize(1);     // size 1 == 8 pixels high
+          matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+          
+          matrix.setCursor(3, 5);    // start at top left, with 8 pixel of spacing
+          uint8_t w = 0;
+          int len1;
+          char *str = "Lunch Time";
+          len1 = strlen(str);
+          for (w=0; w<len1; w++) 
+          {
+            matrix.setTextColor(Wheel(w));
+            matrix.print(str[w]);  
+          }
+          matrix.println();
+          matrix.println();
+          print_phone_num();
+      }
+      else if(cmd == 9)
+      {
+         // fill the screen with 'black'
+          //matrix.fillScreen(matrix.color444(0, 0, 0));
+          matrix.fillScreen(0);
+          // draw some text!
+          matrix.setTextSize(2);     // size 1 == 8 pixels high
+          matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+          
+          matrix.setCursor(8, 2);    // start at top left, with 8 pixel of spacing
+          uint8_t w = 0;
+          int len1;
+          char *str = "Baby On Board";
+          len1 = strlen(str);
+          for (w=0; w<len1; w++) 
+          {
+            if(w == 5) {
+              matrix.println();
+              matrix.setTextSize(1);     // size 1 == 8 pixels high
+              matrix.setCursor(8, 17);    // start at top left, with 8 pixel of spacing
+              delay(1000);
+            }
+            matrix.setTextColor(Wheel(w));
+            matrix.print(str[w]);  
+          }
+          matrix.println();
+          matrix.println();
+          //print_phone_num();
+      }
+      else if(cmd == 1)
+      {
+         // fill the screen with 'black'
+          //matrix.fillScreen(matrix.color444(0, 0, 0));
+          matrix.fillScreen(0);
+          // draw some text!
+          matrix.setTextSize(2);     // size 1 == 8 pixels high
+          matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+          
+          matrix.setCursor(8, 2);    // start at top left, with 8 pixel of spacing
+          uint8_t w = 0;
+          int len1;
+          char *str = "Baby On Board";
+          len1 = strlen(str);
+          for (w=0; w<len1; w++) 
+          {
+            if(w == 5) {
+              matrix.println();
+              matrix.setTextSize(1);     // size 1 == 8 pixels high
+              matrix.setCursor(8, 17);    // start at top left, with 8 pixel of spacing
+            }
+            matrix.setTextColor(Wheel(w));
+            delay(500);
+            matrix.print(str[w]);  
+          }
+          matrix.println();
+          matrix.println();
+          //print_phone_num();
+      }
+      else if(cmd == 2)
+      {
+         // fill the screen with 'black'
+          //matrix.fillScreen(matrix.color444(0, 0, 0));
+          matrix.fillScreen(0);
+          // draw some text!
+          matrix.setTextSize(2);     // size 1 == 8 pixels high
+          matrix.setTextWrap(false); // Don't wrap at end of line - will do ourselves
+          
+          matrix.setCursor(0, 2);    // start at top left, with 8 pixel of spacing
+          uint8_t w = 0;
+          int len1;
+          char *str = "STUDIO GOoooD";
+          len1 = strlen(str);
+          for (w=0; w<len1; w++) 
+          {
+            if(w == 7) {
+              matrix.println();
+              matrix.setTextSize(1);     // size 1 == 8 pixels high
+              matrix.setCursor(8, 17);    // start at top left, with 8 pixel of spacing
+            }
+            matrix.setTextColor(Wheel(w));
+            delay(500);
+            matrix.print(str[w]);  
+          }
+          
+          delay(3000);
+          matrix.println();
+          matrix.fillScreen(0);
+          print_phone_num_gooood();
+      }
+      else if(cmd == 3)
+      {
+         print_arrow();
+      }
+    }    
     else 
     {
        // fill the screen with 'black'
@@ -428,6 +555,102 @@ void loop() {
   
 }
 
+void print_phone_num(void)
+{
+  
+  // print each letter with a rainbow color
+  matrix.setTextColor(matrix.color444(7,0,0));
+  matrix.print('0');
+  matrix.setTextColor(matrix.color444(7,4,0));
+  matrix.print('1');
+  matrix.setTextColor(matrix.color444(7,7,0));
+  matrix.print('0');
+
+  matrix.setTextColor(matrix.color444(4,7,0));
+  matrix.print('4');
+  matrix.setTextColor(matrix.color444(0,7,0));
+  matrix.print('4');
+  matrix.setTextColor(matrix.color444(0,7,7));
+  matrix.print('1');
+  matrix.setTextColor(matrix.color444(0,4,7));
+  matrix.print('8');
+  
+  matrix.setTextColor(matrix.color444(0,0,7));
+  matrix.print('4');
+  matrix.setTextColor(matrix.color444(4,0,7));
+  matrix.print('4');
+  matrix.setTextColor(matrix.color444(7,0,4));
+  matrix.print('5');
+  matrix.setTextColor(matrix.color444(7,0,4));
+  matrix.print('1');  
+}
+
+
+void print_phone_num_gooood(void)
+{
+  for (int i=0; i<3; i++) {
+    matrix.setTextSize(2);  
+    matrix.setCursor(5, 5);
+    
+    // print each letter with a rainbow color
+    matrix.setTextColor(matrix.color444(7,0,0));
+    matrix.print('0');
+    matrix.setTextColor(matrix.color444(7,4,0));
+    matrix.print('1');
+    matrix.setTextColor(matrix.color444(7,7,0));
+    matrix.print('0');
+    delay(2000);      
+    matrix.fillScreen(0);
+  
+    matrix.setCursor(5, 5);
+    matrix.setTextColor(matrix.color444(4,7,0));
+    matrix.print('3');
+    matrix.setTextColor(matrix.color444(0,7,0));
+    matrix.print('2');
+    matrix.setTextColor(matrix.color444(0,7,7));
+    matrix.print('4');
+    matrix.setTextColor(matrix.color444(0,4,7));
+    matrix.print('7');
+    delay(2000); 
+    matrix.fillScreen(0);
+  
+  
+    matrix.setCursor(5, 5);
+    matrix.setTextColor(matrix.color444(0,0,7));
+    matrix.print('2');
+    matrix.setTextColor(matrix.color444(4,0,7));
+    matrix.print('7');
+    matrix.setTextColor(matrix.color444(7,0,4));
+    matrix.print('3');
+    matrix.setTextColor(matrix.color444(7,0,4));
+    matrix.print('4');  
+    delay(2000);      
+    matrix.fillScreen(0);
+  }
+}
+
+
+void print_arrow(void)
+{
+  for (int i=0; i<10; i++) 
+  {
+    matrix.fillScreen(0);
+    // draw some text!
+    matrix.setTextSize(2);     // size 1 == 8 pixels high
+    matrix.setCursor(5, 9);    // start at top left, with 8 pixel of spacing
+    uint8_t w = 0;
+    int len1;
+    char *str = ">>>>>";
+    len1 = strlen(str);
+    for (w=0; w<len1; w++) 
+    {
+      //matrix.setTextColor(Wheel(w));
+      matrix.setTextColor(matrix.color444(15,0,0));
+      delay(200);
+      matrix.print(str[w]);  
+    }
+  }
+}
 
 // Input a value 0 to 24 to get a color value.
 // The colours are a transition r - g - b - back to r.
